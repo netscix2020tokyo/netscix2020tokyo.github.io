25fbb4d241447af86947de4df062831f2fc0ff07
EC2 Default User <ec2-user@ip-172-31-17-141.eu-west-2.compute.internal>
2019-01-21 16:35:26 +0000

    +

--- wp-login.php
+++ wp-login.php
@@ -62,9 +62,6 @@ if(typeof wpOnload=='function')wpOnload();
 	</div>
 
 	
-		<script>
-	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
-	</script>
 		<div class="clear"></div>
 	</body>
 	</html>
